27fd5510ea958ddd276e5a565cce86c1629c81d7
Mohamed Amine DHAINI <m.dhaini5275@uca.ac.ma>
2025-11-29 09:01:16 +0000

    ajout d'exercice 1

---
+++ exercice1.js
@@ -0,0 +1,13 @@
+let x = "150";
+let y = 150;
+let z = true;
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+
+x = Number(x);
+console.log(typeof x);
+
+/*JavaScript est un langage à typage dynamique,
+donc le type d’une variable peut changer pendant l’exécution.*/
