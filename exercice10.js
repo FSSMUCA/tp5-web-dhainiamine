75dd8072ab375ca600be3f7c345a96d26986f61b
Mohamed Amine DHAINI <m.dhaini5275@uca.ac.ma>
2025-11-29 09:34:51 +0000

    répondre aux questions demandées

--- exercice10.js
+++ exercice10.js
@@ -16,3 +16,20 @@ let n = 10;
 
 console.log("Somme itérative de 1 à " + n + " = " + sommeIterative(n));
 console.log("Somme récursive de 1 à " + n + " = " + sommeRecursive(n));
+
+/*analyse
+1 - Quel code est le plus lisible ?
+La version iterative est la plus lisible car une boucle est simple a comprendre
+
+2 - Quel code est le plus performant ?
+La version iterative est plus rapide car il n y a pas plusieurs appels de fonction
+
+3 - La recursion peut-elle poser des problemes ?
+Oui. Si n est trop grand la recursion peut provoquer une erreur (stack overflow)
+car chaque appel prend de la memoire
+
+4 - Quand utiliser une boucle ?
+Pour les taches simples et repetitives
+Les boucles sont plus rapides et plus faciles a lire
+*/
+
